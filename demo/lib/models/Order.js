b8fa57ada2288ae887efa6d228306fb512c16037
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-03-24 16:45:34 -0700

    create all crud models

--- demo/lib/models/Order.js
+++ demo/lib/models/Order.js
@@ -21,4 +21,36 @@ module.exports = class Order {
 
     return new Order(rows[0]);
   }
+
+  static async selectAll() {
+    const { rows } = await pool.query('SELECT * FROM orders');
+
+    return rows;
+  }
+
+  static async selectById(id) {
+    const { rows } = await pool.query('SELECT * FROM orders WHERE id=$1', [id]);
+
+    return new Order(rows[0]);
+  }
+
+  static async update(order) {
+    const { rows } = await pool.query(
+      `UPDATE orders SET quantity=$1 WHERE id=$2
+      RETURNING *`,
+      [order.quantity, order.id]
+    );
+
+    return new Order(rows[0]);
+  }
+
+  static async delete(id) {
+    const { rows } = await pool.query(
+      `DELETE FROM orders WHERE id=$1 
+      RETURNING *`,
+      [id]
+    );
+
+    return new Order(rows[0]);
+  }
 };
